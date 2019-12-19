0629010fd65e34703da13a355190acf49b38057a
FreedomWriter <natalienetworks@gmail.com>
2019-12-18 21:33:38 -0600

    wrapped app in Browser Router as Router:  set up and tested CustomLink

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Globals } from "./globals/GlobalStyles";
+import { CustomLink } from "./components/custom-link/CustomLink.styles";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Globals />
       <h1>Hello From App.js</h1>
+      <CustomLink to="/test">Test</CustomLink>
     </>
   );
 }
