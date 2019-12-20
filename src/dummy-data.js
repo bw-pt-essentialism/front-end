1c339d2cd5b555e1ffc895ef543c0a003f7df5ae
FreedomWriter <natalienetworks@gmail.com>
2019-12-19 21:05:04 -0600

    dummy data created

---
+++ src/dummy-data.js
@@ -0,0 +1,41 @@
+export const user = {
+  name: "Marie Kondo",
+  username: "bringsJoy",
+  password: "password",
+  email: "email@email.com"
+};
+
+export const values = [
+  "Athletic ability",
+  "Art and literature",
+  "Creativity",
+  "discovering, or inventing things to make a difference in the world",
+  "Independence",
+  "Kindness and generosity",
+  "Living in the moment",
+  "Membership in a social group (such as your community, racial group, or school club)",
+  "Music",
+  "My community",
+  "My moral principles",
+  "Nature and the environment",
+  "Relationships with friends and family",
+  "Sense of humor",
+  "Success in my career"
+];
+
+export const valueChoice = {
+  value1: {
+    value: "",
+    explanation: ""
+  },
+  value2: {
+    value: "",
+    explanation: ""
+  },
+  value3: {
+    value: "",
+    explanation: ""
+  }
+};
+
+export const projects = ["project1", "project2", "project3"];
