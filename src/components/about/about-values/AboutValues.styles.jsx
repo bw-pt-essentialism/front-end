65512e90bce61385adfc3d9df5c9fa0650c0c6d7
FreedomWriter <natalienetworks@gmail.com>
2020-01-13 13:17:03 -0600

    deployment layout

--- src/components/about/about-values/AboutValues.styles.jsx
+++ src/components/about/about-values/AboutValues.styles.jsx
@@ -132,7 +132,7 @@ export const AboutValuesHero = styled(Hero)`
 
 export const BottomImg = styled.header`
   margin-top: -5vh;
-  height: 100vh;
+  height: 80vh;
 
   /* ${media.tablet`
  display: none;
